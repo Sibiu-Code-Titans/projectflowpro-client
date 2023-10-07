54b92b62e4eee7a76b2f7fe5c47841c0ac638d37
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2023-10-08 00:07:07 +0300

    removed status

--- src/pages/TaskDetailsPage/components/TaskDetailsSidebar/TaskDetailsSidebar.tsx
+++ src/pages/TaskDetailsPage/components/TaskDetailsSidebar/TaskDetailsSidebar.tsx
@@ -1,16 +1,13 @@
 import TaskDetailsSidebarAssignees from "./TaskDetailsSidebarAssignees/TaskDetailsSidebarAssignees";
 import TaskDetailsSidebarReportingTo from "./TaskDetailsSidebarReportingTo";
-import TaskDetailsSidebarStatus from "./TaskDetailsSidebarStatus";
 import TaskDetailsSidebarWatching from "./TaskDetailsSidebarWatching";
 
 const TaskDetailsSidebar = () => {
   return (
     <div className="flex w-1/4 flex-col gap-y-4 rounded-xl bg-white p-6 shadow-sm">
-      <TaskDetailsSidebarStatus />
       <TaskDetailsSidebarAssignees />
       <TaskDetailsSidebarReportingTo />
       <TaskDetailsSidebarWatching />
-      <TaskDetailsSidebarStatus />
     </div>
   );
 };

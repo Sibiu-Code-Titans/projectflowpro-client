ca7d26fff99f2eba1e3091aca8b07d7d94a937c7
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2023-10-17 20:18:28 +0300

    Updated naming

--- src/pages/TaskDetailsPage/components/TaskDetailsContent/TaskDetailsDescription/TaskDetailsDescription.tsx
+++ src/pages/TaskDetailsPage/components/TaskDetailsContent/TaskDetailsDescription/TaskDetailsDescription.tsx
@@ -1,34 +1,13 @@
 import { useState } from "react";
-import PROButton from "../../../../../common/components/PROButton";
-import TaskDetailsDescriptionEdit from "./TaskDetailsDescriptionEdit/TaskDetailsDescriptionEdit";
+import TaskDetailsDescriptionEditMarkdown from "./TaskDetailsDescriptionEditMarkdown/TaskDetailsDescriptionEditMarkdown";
 
 const TaskDetailsDescription = () => {
   const [isEditMode, setIsEditMode] = useState(false);
 
-  const handleSaveDescription = () => {
-    setIsEditMode(false);
-  };
-
   return (
     <div className="h-full overflow-hidden rounded-xl bg-white px-12 py-6 shadow-sm">
-      <div className="mb-3 flex items-center justify-between font-medium">
-        <div>Description</div>
-        {isEditMode && (
-          <div className="flex items-center gap-x-2 text-sm">
-            <PROButton
-              content="Save"
-              className="bg-purple-500"
-              onClick={() => {}}
-            />
-            <PROButton
-              className="bg-transparent text-zinc-700"
-              content="Cancel"
-              onClick={() => setIsEditMode(false)}
-            />
-          </div>
-        )}
-      </div>
-      <TaskDetailsDescriptionEdit
+      <div className="mb-3 font-medium">Description</div>
+      <TaskDetailsDescriptionEditMarkdown
         isEditMode={isEditMode}
         setIsEditMode={setIsEditMode}
       />
